65b0c228a56d5a5f095c600a5e71d99b0f7e5fca
Stephan Maihöfer <stephan@undev.de>
2022-07-29 18:04:50 +0200

    array?

--- src/Http/Controllers/Backend/Schedules/SlidesController.php
+++ src/Http/Controllers/Backend/Schedules/SlidesController.php
@@ -37,7 +37,7 @@ class SlidesController extends Controller
         $data = $resource->toArrayRecursive();
 
         foreach (Arr::get($data, 'events') as $key => $event) {
-			if(!$event->is_visible) continue;
+			if(!Arr::get($event, 'is_visible')) continue;
 
             $date = Carbon::createFromTimestamp(strtotime(Arr::get($event, 'starts_at')));
             if (! isset($days[$date->format('l')])) {
